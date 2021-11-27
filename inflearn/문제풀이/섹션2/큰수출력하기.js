6bbb7767f957155e880141a7e7604f2f59c3e8ed
Sangho <istrangeho@gmail.com>
2021-11-27 23:22:42 +0900

    section 2 start

---
+++ inflearn/문제풀이/섹션2/큰수출력하기.js
@@ -0,0 +1,13 @@
+
+function solution (arr) {
+    let answer = [];
+    answer.push(arr[0]);
+    for(let x = 1; x < arr.length; ++i) {
+        if(arr[x] > arr[x-1]) {
+            answer.push(arr[x]);
+        }
+    }
+    return answer;
+}
+let arr=[7, 3, 9, 5, 6, 12];
+console.log(solution(arr));
